4ecf84d0dc6d7866df06cb4ef2113ec5f533a721
Lex <Lex@qq.com>
2023-05-12 20:50:02 +0800

    first commit

---
+++ study-project-backend/src/main/java/com/example/StudyProjectBackendApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lex
+ */
+@SpringBootApplication
+public class StudyProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyProjectBackendApplication.class, args);
+    }
+
+}
