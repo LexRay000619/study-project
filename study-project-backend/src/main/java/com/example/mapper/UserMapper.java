a4b5cf1fc470655900d0b02d32ac50d3e2a9326a
Lex <Lex@qq.com>
2023-05-13 18:27:38 +0800

    Send verification code to email and Redis has not been installed yet.

--- study-project-backend/src/main/java/com/example/mapper/UserMapper.java
+++ study-project-backend/src/main/java/com/example/mapper/UserMapper.java
@@ -4,6 +4,9 @@ import com.example.entity.Account;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+/**
+ * @author Lex
+ */
 @Mapper
 public interface UserMapper {
     @Select("select * from db_account where username = #{text} or email = #{text}")
